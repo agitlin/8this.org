0d5b84e6c431bb3dd87e6977f05a95749d0c723c
agitlin <alex.gitlin@gmail.com>
2026-05-30 22:54:28 +0300

    chore: add .gitignore; stop tracking node_modules and dist

--- dist/chunks/8this-beyond-food_DU1yYRrT.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./8this-beyond-food_CXQ1o02z.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- dist/chunks/benevolence-at-home_BdFAtsDQ.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./benevolence-at-home_ChCMvjmh.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
